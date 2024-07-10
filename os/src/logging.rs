92b909787f00325588222ce5240f67dbc68e295d
WithcherTheWhite <1240749052@qq.com>
2024-07-10 17:25:52 +0800

    实现基本执行环境

---
+++ os/src/logging.rs
@@ -0,0 +1,43 @@
+use log::{Level, LevelFilter, Metadata, Record};
+
+use crate::println;
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        let color = match record.metadata().level() {
+            Level::Error => 31,
+            Level::Warn => 93,
+            Level::Info => 34,
+            Level::Debug => 32,
+            Level::Trace => 90,
+        };
+        println!(
+            "\u{1B}[{}m[{:>5}] {}\u{1B}[0m",
+            color,
+            record.level(),
+            record.args(),
+        );
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: SimpleLogger = SimpleLogger;
+
+pub fn init() {
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(match option_env!("LOG") {
+        Some("ERROR") => LevelFilter::Error,
+        Some("WARN") => LevelFilter::Warn,
+        Some("INFO") => LevelFilter::Info,
+        Some("DEBUG") => LevelFilter::Debug,
+        Some("TRACE") => LevelFilter::Trace,
+        _ => LevelFilter::Off,
+    })
+}
---
+++ os/src/main.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+use core::arch::global_asm;
+
+use log::{debug, error, info, trace, warn};
+
+mod console;
+mod lang_item;
+mod logging;
+mod sbi;
+
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+fn rust_main() -> ! {
+    clear_bss();
+    logging::init();
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn skernal();
+        fn ekernal();
+        fn boot_stack_lower_bound();
+        fn boot_stack_top();
+    }
+    error!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    warn!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    debug!(".stack [{:#x}, {:#x})", boot_stack_lower_bound as usize, boot_stack_top as usize);
+    trace!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    println!("kernal [{:#x}, {:#x})", skernal as usize, ekernal as usize);
+    panic!("Shutdown machine");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) })
+}
